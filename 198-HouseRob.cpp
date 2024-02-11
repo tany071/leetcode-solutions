8f79a88c75ab061b9183fff6a04482c2e46eada7
Tanya Sinha <97892373+tany071@users.noreply.github.com>
2024-02-11 21:21:04 +0530

    Dp ques

---
+++ 198-HouseRob.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for (int i =0;i<n;i++){
+            int take = nums[i];
+            if(i>1) take += prev2;
+
+            int notTake = 0 + prev;
+
+            int curi = max(take,notTake);
+            prev2 = prev;
+            prev = curi;
+
+
+        }
+
+        return prev;
+
+        
+    }
+};
